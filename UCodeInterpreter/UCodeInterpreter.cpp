de27fb03111c2194322db46c8026153812320a21
DESKTOP-IV3OEM3\alfud <alfud9078@naver.com>
2021-11-20 17:01:35 +0900

    #Commit04 ui 부분 수정 - Label과 Addr 출력 부분을 CPU 스택 출력에서 명령어 주소 출력 부분으로 옮김 각 tableWidget의 table에 대한 크기 조정

--- UCodeInterpreter/UCodeInterpreter.cpp
+++ UCodeInterpreter/UCodeInterpreter.cpp
@@ -9,6 +9,15 @@ UCodeInterpreter::UCodeInterpreter(QWidget* parent)
 {
     ui.setupUi(this);
 
+    ui.tableWidget->setColumnWidth(0, 80);
+    ui.tableWidget->setColumnWidth(1, 100);
+    ui.tableWidget->setColumnWidth(2, 80);
+    ui.tableWidget->setColumnWidth(3, 80);
+    ui.tableWidget->setColumnWidth(4, 80);
+
+    ui.tableWidget_2->setColumnWidth(0, 230);
+    ui.tableWidget_2->setColumnWidth(1, 230);
+
     connect(ui.pushButton_3, &QPushButton::clicked, this, &UCodeInterpreter::On_ReadUcoButton_Clicked);
     connect(ui.pushButton_6, &QPushButton::clicked, this, &UCodeInterpreter::On_ExitButton_Clicked);
 }
@@ -189,7 +198,7 @@ void UCodeInterpreter::Execute()
     for (int i = 0; i < Instructions.size(); i++)
     {
         int opcodeNum = 0;
-        for (int j= 0; j < 40; j++)
+        for (int j= 0; j < NO_OPCODE; j++)
         {
             if (opcodeName[j] == Instructions[i].inst)
             {
